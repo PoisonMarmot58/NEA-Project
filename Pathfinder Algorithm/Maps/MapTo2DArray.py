ec7c25932d3cccaa7f30d66801e688fe1bc541ad
PoisonMarmot58 <PoisonMarmot58@users.noreply.github.com>
2025-11-01 11:51:17 +0000

    map of europe converted to 2d array

--- Pathfinder Algorithm/Maps/MapTo2DArray.py
+++ Pathfinder Algorithm/Maps/MapTo2DArray.py
@@ -1,8 +1,14 @@
 from PIL import Image
 import numpy as np
 
-image = Image.open("C/Users/isaac/OneDrive/Desktop/NEA Project new/NEA-Project-2/Pathfinder Algorithm/Maps/MapOfEuropeNonNamed.png")
+def closest_colour(rgb, colour_map, tolerance=10):
+    for colour, value in colour_map.items():
+        if all(abs(int(c1) - int(c2)) <= tolerance for c1, c2 in zip(rgb, colour)):
+            return value
+    return -1
 
+
+image = Image.open(r"C:\Users\isaac\OneDrive\Desktop\NEA Project new\NEA-Project-2\Pathfinder Algorithm\Maps\MapOfEuropeNonNamed.png")
 imageArray = np.array(image)
 
 colourMap = {
@@ -12,10 +18,10 @@ colourMap = {
 }
 
 mappedArray = np.zeros((imageArray.shape[0], imageArray.shape[1]), dtype=int)
-
+print("processing")
 for i in range(imageArray.shape[0]):
     for j in range(imageArray.shape[1]):
         rgb = tuple(imageArray[i, j])
-        mappedArray[i, j] = colourMap.get(rgb, -1)
-
-print(colourMap)
+        mappedArray[i, j] = closest_colour(rgb, colourMap)
+print("running")
+np.save(r"C:\Users\isaac\OneDrive\Desktop\NEA Project new\NEA-Project-2\Pathfinder Algorithm\Maps", mappedArray)
