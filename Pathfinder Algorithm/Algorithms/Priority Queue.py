0f9da551f62e878f017f15e6b75d6c85a2312be1
PoisonMarmot58 <PoisonMarmot58@users.noreply.github.com>
2026-01-13 16:38:22 +0000

    grid creator finalized and improved organisation

---
+++ Pathfinder Algorithm/Algorithms/Priority Queue.py
@@ -0,0 +1,22 @@
+class PriorityQueue:
+    def __init__ (self):
+        self.queue = []
+
+    def push(self, priority, value):
+        self.queue.append((priority, value))
+        self.queue.sort(key=lambda x: x[0])
+
+    def pop(self):
+        if not self.queue:
+            print("Error: Queue is empty.")
+            return None
+        else:
+            priority,value = self.queue.pop(0)
+            return value
+        
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def size(self):
+        return len(self.queue)
+    
